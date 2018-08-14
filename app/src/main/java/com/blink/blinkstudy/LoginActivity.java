11c700dbf8fa8a1434decc2c148c9c3a18fec8db
BakJH <bakjh.6280@gmail.com>
2018-08-14 19:03:21 +0900

    회원가입 & 로그인 주요 로직 구현 (1)

--- app/src/main/java/com/blink/blinkstudy/LoginActivity.java
+++ app/src/main/java/com/blink/blinkstudy/LoginActivity.java
@@ -1,15 +1,53 @@
 package com.blink.blinkstudy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+    private EditText id;
+    private EditText password;
+    private Button registerBtn;
+    private Button loginBtn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.login);
+
+        registerBtn = (Button) findViewById(R.id.registBtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        id = (EditText) findViewById(R.id.loginID);
+        password = (EditText) findViewById(R.id.loginPassword);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (id.getText().toString().length() == 0) {
+                    Toast.makeText(LoginActivity.this, "ID를 입력하세요", Toast.LENGTH_SHORT).show();
+                    id.requestFocus();
+                    return;
+                }
+
+                if (password.getText().toString().length() == 0) {
+                    Toast.makeText(LoginActivity.this, "비밀번호를 입력하세요", Toast.LENGTH_SHORT).show();
+                    password.requestFocus();
+                    return;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/blink/blinkstudy/RegistrationActivity.java
+++ app/src/main/java/com/blink/blinkstudy/RegistrationActivity.java
@@ -1,7 +1,6 @@
 package com.blink.blinkstudy;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
@@ -12,16 +11,71 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class RegistrationActivity extends Activity{
-    private Button btnDone;
-    private Button btnCancel;
 
-    private EditText ID;
-    private EditText Password;
-    private EditText PasswordConfirm;
+    private EditText id;
+    private EditText password;
+    private EditText passwordConfirm;
+    private Button nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.regist);
+
+        id = (EditText)findViewById(R.id.registID);
+        password = (EditText)findViewById(R.id.registPassword);
+        passwordConfirm = (EditText)findViewById(R.id.PasswordConfirm);
+        nextBtn = (Button)findViewById(R.id.nextBtn);
+
+        /// 비밀번호 일치 검사 ///
+        passwordConfirm.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String original = password.getText().toString();
+                String confirm = passwordConfirm.getText().toString();
+
+                if(original.equals(confirm)) {
+                    password.setTextColor(Color.GREEN);
+                    passwordConfirm.setTextColor(Color.GREEN);
+                } else {
+                    password.setTextColor(Color.RED);
+                    passwordConfirm.setTextColor(Color.RED);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        /// 입력 여부 확인 ///
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if( id.getText().toString().length() == 0 ) {
+                    Toast.makeText(RegistrationActivity.this, "ID를 입력하세요", Toast.LENGTH_SHORT).show();
+                    id.requestFocus();
+                    return;
+                }
+
+                if( password.getText().toString().length() == 0 ) {
+                    Toast.makeText(RegistrationActivity.this, "비밀번호를 입력하세요", Toast.LENGTH_SHORT).show();
+                    password.requestFocus();
+                    return;
+                }
+
+                if( passwordConfirm.getText().toString().length() == 0 ) {
+                    Toast.makeText(RegistrationActivity.this, "비밀번호 확인을 입력하세요", Toast.LENGTH_SHORT).show();
+                    passwordConfirm.requestFocus();
+                    return;
+                }
+            }
+        });
     }
 }
