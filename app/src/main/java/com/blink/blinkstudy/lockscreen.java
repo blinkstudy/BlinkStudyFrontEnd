23f02e686cc99a4cbb699a803eae2c88b45b7d8b
JoNamHyun <skagus5907@naver.com>
2018-09-16 23:00:10 +0900

    잠금화면 기능
    문제 띄우기, 4지선다 선택지 띄우기, 정답체크, 4지선다 중복체크 구현완료

--- app/src/main/java/com/blink/blinkstudy/lockscreen.java
+++ app/src/main/java/com/blink/blinkstudy/lockscreen.java
@@ -1,13 +1,17 @@
 package com.example.dsm2017.lockscreen;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.dsm2017.teamproject.ListActivity;
 import com.example.dsm2017.teamproject.R;
+import com.example.dsm2017.teamproject.writing;
 
 import java.util.Random;
 
@@ -15,10 +19,8 @@ public class lockscreen extends AppCompatActivity {
 
     TextView tv_question,btn_a1,btn_a2,btn_a3,btn_a4;
     TextView tv_advertisement;
-    int [] answer_array = new int[4];
-    String answer[]={"1","2","3","4"};
-    String correct="1";
-
+    int correct_c=0;
+    int wrong_c=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +35,43 @@ public class lockscreen extends AppCompatActivity {
         btn_a3=findViewById(R.id.btn_a3);
         btn_a4=findViewById(R.id.btn_a4);
 
+        //정답체크를 위한 quest배열의 index 값
+        final int index[]=new int[4];
 
+        //문제와 정답을 파싱해오는 배열
+        final String[] quest = getResources().getStringArray(R.array.q_answer1);
+
+        //중복체크
+        for(int i=0; i<4; i++) {
+            index[i]=random.nextInt(4)+1;
+        }
+        int cnt=0;
+        while (cnt == 0) {
+            index[0] = random.nextInt(4)+1;
+            index[1] = random.nextInt(4)+1;
+            index[2] = random.nextInt(4)+1;
+            index[3] = random.nextInt(4)+1;
+
+            if (index[0] == index[1]) {
+                index[1] = random.nextInt(4)+1;
+                continue;
+            }
+            if (index[0] == index[2] || index[1] == index[2]) {
+                index[2] = random.nextInt(4)+1;
+                continue;
+            }
+            if (index[0] == index[3] || index[1] == index[3] || index[2] == index[3]) {
+                index[3] = random.nextInt(4)+1;
+                continue;
+            }
+            cnt++;
+        }
+
+        tv_question.setText(quest[0]);
+        btn_a1.setText(quest[index[0]]);
+        btn_a2.setText(quest[index[1]]);
+        btn_a3.setText(quest[index[2]]);
+        btn_a4.setText(quest[index[3]]);
 
         tv_question.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,58 +103,78 @@ public class lockscreen extends AppCompatActivity {
         btn_a1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (quest[index[0]].equals(quest[1]) == true)
+                {
+                    correct_c++;
+                    Toast.makeText(getApplicationContext(), "정답입니다!.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
 
-
-
+                else
+                {
+                    wrong_c++;
+                    Toast.makeText(getApplicationContext(), "오답입니다!.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
             }
         });
 
         btn_a2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (quest[index[1]].equals(quest[1]) == true)
+                {
+                    correct_c++;
+                    Toast.makeText(getApplicationContext(), "정답입니다!.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
 
+                else
+                {
+                    wrong_c++;
+                    Toast.makeText(getApplicationContext(), "오답입니다!.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
             }
         });
 
         btn_a3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (quest[index[2]].equals(quest[1]) == true)
+                {
+                    correct_c++;
+                    Toast.makeText(getApplicationContext(), "정답입니다!.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
 
+                else
+                {
+                    wrong_c++;
+                    Toast.makeText(getApplicationContext(), "오답입니다!.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
             }
         });
 
         btn_a4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (quest[index[3]].equals(quest[1]) == true)
+                {
+                    correct_c++;
+                    Toast.makeText(getApplicationContext(), "정답입니다!.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
 
+                else
+                {
+                    wrong_c++;
+                    Toast.makeText(getApplicationContext(), "오답입니다!.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
             }
         });
     }
-
-    /*
-    @Override
-    public void onBackPressed() {
-        //Dialog 빌더 생성
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        //빌더 속성에서 타이틀 설정
-        builder.setTitle("스킵하시겠습니까?");
-        //빌더 속성에서 메세지 설정
-        builder.setMessage("문제를 스킵하시겠습니까?\n" + "스킵 시 오답처리 됩니다.");
-
-        //확인 버튼(긍정 버튼) 이름과 클릭 이벤트 처리
-        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                finish();
-
-            }
-        });
-
-        //취소 버튼(부정 버튼) 이름 설정 및 클릭 이벤트는 null로 설정
-        builder.setNegativeButton("취소", null);
-        //Dialog 보여주기
-        builder.show();
-    }
-    */
 
 }
